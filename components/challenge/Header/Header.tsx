a8f22666e7f521ecb38dfef4a7a070eb3be945b4
Daria Artemova <airadavometra@gmail.com>
2023-04-22 21:05:24 +0200

    add cart items count to header

--- components/challenge/Header/Header.tsx
+++ components/challenge/Header/Header.tsx
@@ -4,6 +4,8 @@ import { NavigationItem } from "@/types/navigationItem";
 import Link from "next/link";
 import WidthContainer from "../../WidthContainer/WidthContainer";
 import s from "./Header.module.css";
+import { useChallengeStore } from "@/state/useChallenge";
+import { useMemo } from "react";
 
 type HeaderProps = {
   navigation: NavigationItem[];
@@ -11,6 +13,13 @@ type HeaderProps = {
 };
 
 export const Header = ({ navigation, onMenuItemClick }: HeaderProps) => {
+  const cart = useChallengeStore((state) => state.cart);
+
+  const cartCount = useMemo(
+    () => cart.reduce((sum, item) => sum + item.quantity, 0),
+    [cart]
+  );
+
   return (
     <header className={s.header}>
       <WidthContainer className={s.widthContainer}>
@@ -33,7 +42,10 @@ export const Header = ({ navigation, onMenuItemClick }: HeaderProps) => {
           </ul>
         </nav>
         <Link href={"/challenge/cart"} className={s.cartButton}>
-          <Cart />
+          <div className={s.cartContainer}>
+            <Cart />
+            <span className={s.cartCount}>{cartCount}</span>
+          </div>
           <VisuallyHidden>Cart</VisuallyHidden>
         </Link>
       </WidthContainer>
