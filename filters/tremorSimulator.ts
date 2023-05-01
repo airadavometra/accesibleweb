262e8e54d16e3bd0dce6435f1382d54a085b2a29
Daria Artemova <airadavometra@gmail.com>
2023-05-01 15:58:36 +0200

    prevent click on touchscreens

--- filters/tremorSimulator.ts
+++ filters/tremorSimulator.ts
@@ -19,6 +19,15 @@ const tremor = (cursor: HTMLElement) => {
   );
 };
 
+const click = (e: Event) => {
+  const randomNum = Math.floor(Math.random() * 5) + 1;
+  if (randomNum !== 5) {
+    e.stopPropagation();
+    e.preventDefault();
+    return false;
+  }
+};
+
 export const simulateTremor = () => {
   const cursor = document.querySelector(".cursor") as HTMLElement;
   if (cursor) {
@@ -29,18 +38,8 @@ export const simulateTremor = () => {
       }
     });
 
-    document.addEventListener(
-      "click",
-      (e: MouseEvent) => {
-        const randomNum = Math.floor(Math.random() * 5) + 1;
-        if (randomNum !== 5) {
-          e.stopPropagation();
-          e.preventDefault();
-          return false;
-        }
-      },
-      true
-    );
+    document.addEventListener("click", click, true);
+    document.addEventListener("touchstart", click, true);
 
     tremor(cursor);
   }
