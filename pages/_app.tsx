26f1d542c48ced69205b6fe52baebad6f2f56fc0
Daria Artemova <airadavometra@gmail.com>
2023-05-22 14:57:11 +0200

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { Layout as MainLayout } from "@/components/main/Layout/Layout";
 import { Layout as ChallengeLayout } from "@/components/challenge/Layout/Layout";
 import { Layout as ChallengeAccessibleLayout } from "@/components/challenge/accessible/Layout/Layout";
@@ -11,8 +12,11 @@ export default function App({ Component, pageProps, router }: AppProps) {
     ? ChallengeLayout
     : MainLayout;
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
