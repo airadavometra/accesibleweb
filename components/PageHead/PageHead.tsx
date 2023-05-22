f4e629ee4bb903f51b0505b06a01339ac5f89695
Daria Artemova <airadavometra@gmail.com>
2023-05-22 16:37:45 +0200

    add og tags

--- components/PageHead/PageHead.tsx
+++ components/PageHead/PageHead.tsx
@@ -17,7 +17,15 @@ export const PageHead = () => (
       />
       <meta name="keywords" content="accessibility UI UX web" />
       <meta property="og:title" content="Access denied?" />
+      <meta property="og:url" content="https://access-denied.vercel.app/" />
       <meta property="og:image" content="/share.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="672" />
+      <meta property="og:type" content="website" />
+      <meta
+        name="og:description"
+        content="This project focuses on highlighting the importance of web accessibility through interactive experiences. It educates about the online challenges faced by people with diverse abilities, emphasizing that websites should be inherently accessible."
+      />
     </NextHead>
     {process.env.NODE_ENV === "production" &&
       process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && <GoogleAnalytics />}
